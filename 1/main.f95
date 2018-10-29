9dbd407b557ed57dde256b2f563ddb1b6443a499
catmagic <catmagic000@gmail.com>
2018-10-29 14:42:24 +0300

    xenm xenm

--- 1/main.f95
+++ 1/main.f95
@@ -2,8 +2,8 @@
 ! By WQY
 program main
     integer,parameter::n=2
-    real T_f,T_k,P_psi,P_bar
-    real T(2),T_crit(2),T_ri(2)
+    real T_f,T_k,P_psi,P_bar,Omega_A_0
+    real T(2),T_crit(2),T_ri(2),Omega_A(2),omega(2)
     T_f     =   500.
     P_psi   =   1400.
     T=(/1.,1./)
@@ -22,20 +22,18 @@ end
 
 subroutine covert_F_to_K(T_f,T_k)
     real T_f,T_k
-
     T_k = (T_f - 32) * 5. / 9. + 273.15
 end subroutine
 
 subroutine covert_psi_to_bar(P_psi,P_bar)
     real P_psi,P_bar
-
     P_bar = P_psi/14.504
 end subroutine
 
 subroutine T_ri_calc(T,T_crit,T_ri,n)
     integer n
     real ::T_ri(n),T(n),T_crit(n)
-     T_ri=T/T_crit
+    T_ri=T/T_crit
 end subroutine
 
 subroutine omega_a_calc(T_ri,omega,n,Omega_A,Omega_A_0)
