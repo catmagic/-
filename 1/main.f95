783e1a836369f9d6279256062cacb6dff6ee90cd
catmagic <catmagic000@gmail.com>
2018-10-29 11:37:34 +0300

    добавлен перевод давления из пси в бары

--- 1/main.f95
+++ 1/main.f95
@@ -2,11 +2,15 @@
 ! By WQY
 program main
 
-    real T_f,T_k
-    t_f=500.
+    real T_f,T_k,P_psi,P_bar
+    T_f     =   500.
+    P_psi   =   1400.
     call covert_F_to_K(T_f,T_k)
     write(*, *)T_f
     write(*, *)T_k
+    call covert_psi_to_bar(P_psi,P_bar)
+    write(*, *)P_psi
+    write(*, *)P_bar
 end
 
 subroutine covert_F_to_K(T_f,T_k)
@@ -14,4 +18,18 @@ subroutine covert_F_to_K(T_f,T_k)
     intent(in)  T_f
     intent(out) T_k
     T_k = (T_f - 32) * 5. / 9. + 273.15
+end subroutine
+
+subroutine covert_psi_to_bar(P_psi,P_bar)
+    real P_psi,P_bar
+    intent(in)  P_psi
+    intent(out) P_bar
+    P_bar = P_psi/14.504
+end subroutine
+subroutine omega_a_calc(T_ri,omega,n,Omega_A)
+    integer, parameter :: n
+    real ::T_ri(n),omega(n),Omega_A(n)
+    intent(in)  n,T_ri,omega
+    intent(out) P_bar
+    P_bar = P_psi/14.504
 end subroutine
