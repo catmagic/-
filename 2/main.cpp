794040fee6619f8a63a37356eac3a3e987078531
catmagic <catmagic000@gmail.com>
2018-11-02 23:03:43 +0300

    Добавлены конвертация в кельвени и в бары

--- 2/main.cpp
+++ 2/main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+double convert_F_to_K(double T_f)
+{
+    return (T_f-32.0)*5.0/9.0+273.15;
+}
+
+double convert_psi_to_bar(double P_psi)
+{
+    return P_psi/14.504;
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
