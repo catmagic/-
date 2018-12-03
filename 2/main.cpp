6c6adcafced66c5b8d147a3cb132f6e629180b03
catmagic <catmagic000@gmail.com>
2018-12-03 08:20:50 +0300

    Почти дописанная программа Исправлено некоторое количество багов

--- 2/main.cpp
+++ 2/main.cpp
@@ -223,13 +223,15 @@ void calc_phi(double C,double * S_i,double *B_i,double* Phi_i,double m1,double m
     {
         Ln_phi_i[i]=log(C/(Z-B))+M*A/((m1-m2)*B)*(2.0*S_i[i]/A-B_i[i]/B)+B_i[i]*(Z-C)/B;
         Phi_i[i]=exp(Ln_phi_i[i]);
+        cout<<Z<<'\n';
     }
 }
-void coeff(double E2,double E1,double E0,double m1,double m2,double A,double B,double C)
+void coeff(double &E2,double &E1,double &E0,double m1,double m2,double A,double B,double C)
 {
     E2=(m1+m2)*B-(B+C);
-    E1=A+m1*m2*B*B-(m1+m2)*B*(B+C);
+    E1=A+m1*m2*B*B;-(m1+m2)*B*(B+C);
     E0=-A*B-m1*m2*B*B*(B+C);
+   cout<<"E   "<<E2<<" "<<E1<<" "<<E0<<'\n';
 }
 void calc_c(double *c_i,double c,int n)
 {
@@ -238,6 +240,7 @@ void calc_c(double *c_i,double c,int n)
     {
         c+=c_i[i];
     }
+    cout<<"C "<<c;
 }
 void calc_si(double* si,double** A_ij,double *c,int n)
 {
@@ -363,7 +366,7 @@ void K_calk(double* K_i,double* tetta_i,int n)
 }
 void Gauss(double** A,double* B,double* x,int n)
 {
-    int W; cin>>W;
+
     for(int i=0;i<n;i++)
     {
 
@@ -438,31 +441,17 @@ int main()
     double E2_liq,E1_liq,E0_liq,z_liq;
     double E2_gas,E1_gas,E0_gas,z_gas;
     int n,count_root_gas,count_root_liq;
-    double *phi_liq_i,*phi_gas_i;
-    A_linar     =   (double**)malloc(3*sizeof(double*));
-    A_linar[0]    =   (double*)malloc(3*sizeof(double));
-    A_linar[1]  =   (double*)malloc(3*sizeof(double));
-    A_linar[2]=   (double*)malloc(3*sizeof(double));
-    B_linar=   (double*)malloc(3*sizeof(double));
-    x_solve=   (double*)malloc(3*sizeof(double));
-    A_linar[0][0]=1;
-    A_linar[0][1]=2;
-    A_linar[0][2]=3;
-    A_linar[1][0]=0;
-    A_linar[1][1]=2;
-    A_linar[1][2]=3;
-    A_linar[2][0]=0;
-    A_linar[2][1]=0;
-    A_linar[2][2]=3;
-    B_linar[0]=1;
-    B_linar[1]=1;
-    B_linar[2]=1;
-    x_solve[0]=0.0;
-    x_solve[1]=0.0;
-    x_solve[2]=0.0;
-    Gauss(A_linar,B_linar,x_solve,3);
-    cout<<"Gauss: "<<x_solve[0]<<"   "<<x_solve[1]<<"    "<<x_solve[2]<<'\n';
+    int templ;
+    double *phi_liq_i,*phi_gas_i,*dz_dc_liq,*dz_dc_gas,**d_ln_phi_dK_liq,**d_ln_phi_dK_gas,**dx_dK,**dy_dK;
+
+
     data>>n>>T>>P;
+
+
+    dz_dc_liq   =   (double*)malloc(n*sizeof(double));
+    dz_dc_gas   =   (double*)malloc(n*sizeof(double));
+    B_linar     =   (double*)malloc(n*sizeof(double));
+    x_solve     =   (double*)malloc(n*sizeof(double));
     s_liq_i     =   (double*)malloc(n*sizeof(double));
     s_gas_i     =   (double*)malloc(n*sizeof(double));
     phi_liq_i   =   (double*)malloc(n*sizeof(double));
@@ -484,8 +473,15 @@ int main()
     c           =   (double*)malloc(n*sizeof(double));
     k_i         =   (double*)malloc(n*sizeof(double));
 
-    A_ij        =   (double**)malloc(n*sizeof(double*));
-    Betta       =   (double**)malloc(n*sizeof(double*));
+
+
+    A_ij                =   (double**)malloc(n*sizeof(double*));
+    Betta               =   (double**)malloc(n*sizeof(double*));
+    A_linar             =   (double**)malloc(n*sizeof(double*));
+    dy_dK               =   (double**)malloc(n*sizeof(double));
+    dx_dK               =   (double**)malloc(n*sizeof(double));
+    d_ln_phi_dK_liq     =   (double**)malloc(n*sizeof(double*));
+    d_ln_phi_dK_gas     =   (double**)malloc(n*sizeof(double*));
     int i=0,j=0;
     for(i=0;i<n;i++)
     {
@@ -493,10 +489,17 @@ int main()
         t[i]=T;
         p[i]=P;
         k_i[i]=(i<n/2)?(0.5):(2.0);
+        cout<<k_i[i]<<z[i]<<'\n';
 
-        A_ij[i]         =   (double*)malloc(n*sizeof(double));
-        Betta[i]        =   (double*)malloc(n*sizeof(double));
+        A_ij[i]                 =   (double*)malloc(n*sizeof(double));
+        Betta[i]                =   (double*)malloc(n*sizeof(double));
+        A_linar[i]              =   (double*)malloc(n*sizeof(double));
+        dy_dK[i]                =   (double*)malloc(n*sizeof(double));
+        dx_dK[i]                =   (double*)malloc(n*sizeof(double));
+        d_ln_phi_dK_liq[i]      =   (double*)malloc(n*sizeof(double*));
+        d_ln_phi_dK_gas[i]      =   (double*)malloc(n*sizeof(double*));
     }
+
     normalization_P(p,p_crit,p_r,n);
     normalization_T(t,t_crit,t_r,n);
 
@@ -514,7 +517,6 @@ int main()
             data>>Betta[i][j];
         }
     }
-
     calc_B_i(Omega_B,p_r,t_r,B_i,n);
     calc_A_i(Omega_A,p_r,t_r,A_i,n);
     calc_A_i_j(Betta,A_i,A_ij,n);
@@ -526,26 +528,51 @@ int main()
     calc_c(y,C_gas,n);
     coeff(E2_liq,E1_liq,E0_liq,m1,m2,A_liq,B_liq,C_liq);
     coeff(E2_gas,E1_gas,E0_gas,m1,m2,A_gas,B_gas,C_gas);
+    cout<<"E   "<<E2_liq<<" "<<E1_liq<<" "<<E0_liq<<'\n';
+    cout<<"E   "<<E2_gas<<" "<<E1_gas<<" "<<E0_gas<<'\n';
     cube_solver(E2_liq,E1_liq,E0_liq,root_liq,count_root_liq);
     cube_solver(E2_gas,E1_gas,E0_gas,root_gas,count_root_gas);
     z_liq=max(max(root_liq[0],root_liq[1]),root_liq[2]);
-    if((z_liq<root_liq[0])&&(root_liq[0]>B_liq)){z_liq=root_liq[0];}
-    if((z_liq<root_liq[1])&&(root_liq[1]>B_liq)){z_liq=root_liq[1];}
-    if((z_liq<root_liq[2])&&(root_liq[2]>B_liq)){z_liq=root_liq[2];}
+    if((z_liq>root_liq[0])&&(root_liq[0]>B_liq)){z_liq=root_liq[0];}
+    if((z_liq>root_liq[1])&&(root_liq[1]>B_liq)){z_liq=root_liq[1];}
+    if((z_liq>root_liq[2])&&(root_liq[2]>B_liq)){z_liq=root_liq[2];}
     //z min positive root
     z_gas=max(max(root_gas[0],root_gas[1]),root_gas[2]);
     calc_si(s_liq_i,A_ij,x,n);
     calc_si(s_gas_i,A_ij,y,n);
+    cout<<"Z   "<<root_gas[0]<<" "<<root_gas[1]<<" "<<root_gas[2]<<'\n';
+    cout<<"E   "<<E2_liq<<" "<<E1_liq<<" "<<E0_liq<<'\n';
     calc_phi(C_liq,s_liq_i,B_i,phi_liq_i,m1,m2,z_liq,B_liq,A_liq,n);
     calc_phi(C_gas,s_gas_i,B_i,phi_gas_i,m1,m2,z_gas,B_gas,A_gas,n);
     double kriterij=0.0;
     for(int k=0;k<n;k++)
     {
-        k+=abs((x[i]*phi_liq_i[i])/(y[i]*phi_liq_i[i])-1.0);
+        kriterij+=abs((x[i]*phi_liq_i[i])/(y[i]*phi_gas_i[i])-1.0);
     }
+    cout<<"Kriterij: "<<kriterij<<'\n';
     if(kriterij>EPSILON)
     {
+        practical_Z(z_liq,m1,m2,E2_liq,E1_liq,A_liq,B_liq,C_liq,B_i,s_liq_i,dz_dc_liq,n);
+        practical_Z(z_gas,m1,m2,E2_gas,E1_gas,A_gas,B_gas,C_gas,B_i,s_gas_i,dz_dc_gas,n);
+        practical_liq(z,k_i,v,dx_dK,n);
+        practical_gas(z,k_i,v,dy_dK,n);
+        cout<<"phi_i:\n";
+    for(int w=0;w<n;w++)
+    {cout<<phi_liq_i[w]<<"  "<<phi_gas_i[w]<<'\n';
     }
+    cin>>templ;
+        practical_ln_phi(m1,m2,A_liq,B_liq,C_liq,z_liq,B_i,s_liq_i,dz_dc_liq,dx_dK,d_ln_phi_dK_liq,A_ij,n);
+        practical_ln_phi(m1,m2,A_gas,B_gas,C_gas,z_gas,B_i,s_gas_i,dz_dc_gas,dy_dK,d_ln_phi_dK_gas,A_ij,n);
+        A_linar_calc(A_linar,d_ln_phi_dK_liq,d_ln_phi_dK_gas,k_i,n);
+        B_linar_calc(B_linar,phi_liq_i,phi_gas_i,k_i,n);
+        Gauss(A_linar,B_linar,x_solve,n);
+        K_calk(k_i,x_solve,n);
+    }
+    cout<<"k_i:\n";
+    for(int w=0;w<n;w++)
+    {cout<<k_i[w]<<z[w]<<'\n';
+    }cin>>templ;
+    cout<<"Kriterij: "<<kriterij<<'\n';
     complex<double> a,root[3];
     double root_d[3];
     a.real()=0.0;
